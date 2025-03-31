fcefe87db66bd28ad00334998fa4ddcaf7cfe14e
Chris Yoon <chris.cw.yoon@gmail.com>
2025-03-31 20:36:42 +0900

    EgovHello http://localhost:30086/hello otel collector - loki 로그 확인

---
+++ EgovHello/src/main/java/egovframework/com/hello/web/HelloController.java
@@ -0,0 +1,20 @@
+package egovframework.com.hello.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+    
+    @GetMapping
+    public String hello() {
+        LOGGER.info("Hello API called");
+        return "Hello from EgovFramework!";
+    }
+}
